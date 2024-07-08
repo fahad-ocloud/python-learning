c35f5544710a18fe2704476803b1238ea40ecbaa
fahad-ocloud <fahad.asif@ocloudsolutions.net>
2024-07-08 19:39:54 +0500

    type-conversion

--- type-conversion.py
+++ type-conversion.py
@@ -1,3 +1,7 @@
-birth = input("Birth year: ")
-age = 2019 - int(birth)
-print(age)
+# birth = input("Birth year: ")
+# age = 2024 - int(birth)
+# print(age)
+
+weight_kgs = input("Weight(kg): ")
+weight_lbs = int(weight_kgs)/0.45
+print("(lbs)"+ str(weight_lbs))
