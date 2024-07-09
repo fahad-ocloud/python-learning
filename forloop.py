265f10b09d30a1cf9891de7d90ceafc07dd3617f
fahad-ocloud <fahad.asif@ocloudsolutions.net>
2024-07-09 12:28:39 +0500

    for lopp done

---
+++ forloop.py
@@ -0,0 +1,13 @@
+prices = [10,20,30]
+total = 0
+for item in prices:
+    total+=item
+print(f'Total price = {total}')
+
+numbers = [5,2,5,2,2]
+
+for item in numbers:
+    output=''
+    for x in range(item):
+        output+='x'
+    print(output)
