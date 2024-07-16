50f1e45db2eb1bd79161ecdb58d5e1604191fe4a
fahad-ocloud <fahad.asif@ocloudsolutions.net>
2024-07-16 18:59:10 +0500

    changes

--- practice_tasks/polymorphism/persons/employee.py
+++
@@ -1,7 +0,0 @@
-from .parent import IPerson
-class Employee(IPerson):
-    def __init__(self,name,age, salary):
-        super().__init__(name,age,salary)
-    
-    def work(self):
-        print(f"{self.name} works in Factory")
